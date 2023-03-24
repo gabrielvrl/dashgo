a1b5dfd110b288fbde83d7e6c2f9f1978cd74777
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2023-03-23 21:55:13 -0300

    Listagem responsiva

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,11 +1,16 @@
 import Pagination from "@/components/Pagination";
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text, useBreakpointValue } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 
 import { Header } from '../../components/Header';
 import { Sidebar } from '../../components/Sidebar';
 
 export default function UserList() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
+
   return (
     <Box>
       <Header />
@@ -25,18 +30,18 @@ export default function UserList() {
           <Table colorScheme="whiteAlpha">
             <Thead>
               <Tr>
-                <Th px="6" color="gray.300" width="8">
+                <Th px={["4", "4", "6"]} color="gray.300" width="8">
                   <Checkbox colorScheme="pink" />
                 </Th>
                 <Th>Usuário</Th>
-                <Th>Data de cadastro</Th>
+                {isWideVersion && <Th>Data de cadastro</Th>}
                 <Th width="8"></Th>
               </Tr>
             </Thead>
 
             <Tbody>
               <Tr>
-                <Td px="6">
+                <Td px={["4", "4", "6"]}>
                   <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
@@ -45,13 +50,67 @@ export default function UserList() {
                     <Text fontSize="sm" color="gray.300">gabriel@email.com</Text>
                   </Box>
                 </Td>
-                <Td>04 de Abril, 2021</Td>
+                {isWideVersion && <Td>04 de Abril, 2021</Td>}
 
-                <Td>
-                  <Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="16" />} >
-                    Editar
-                  </Button>
+
+                {
+                  isWideVersion && 
+                  (
+                    <Td>
+                      <Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="16" />} >
+                        Editar
+                      </Button>
+                    </Td>
+                  )
+                }
+              </Tr>
+
+              <Tr>
+                <Td px={["4", "4", "6"]}>
+                  <Checkbox colorScheme="pink" />
                 </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Gabriel Varela</Text>
+                    <Text fontSize="sm" color="gray.300">gabriel@email.com</Text>
+                  </Box>
+                </Td>
+                {isWideVersion && <Td>04 de Abril, 2021</Td>}
+
+                {
+                  isWideVersion && 
+                  (
+                    <Td>
+                      <Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="16" />} >
+                        Editar
+                      </Button>
+                    </Td>
+                  )
+                }
+              </Tr>
+
+              <Tr>
+                <Td px={["4", "4", "6"]}>
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Gabriel Varela</Text>
+                    <Text fontSize="sm" color="gray.300">gabriel@email.com</Text>
+                  </Box>
+                </Td>
+                {isWideVersion && <Td>04 de Abril, 2021</Td>}
+
+                {
+                  isWideVersion && 
+                  (
+                    <Td>
+                      <Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="16" />} >
+                        Editar
+                      </Button>
+                    </Td>
+                  )
+                }
               </Tr>
             </Tbody>
           </Table>
