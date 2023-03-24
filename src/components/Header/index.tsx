c48fde7e9d74ae4b39e686c23c72c0759f8ad7bb
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2023-03-23 21:45:52 -0300

    Sidebar responsiva

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,14 @@
-import { Flex, useBreakpointValue } from '@chakra-ui/react';
+import { useSidebarDrawer } from '@/contexts/SidebarDrawerContext';
+import { Flex, Icon, IconButton, useBreakpointValue } from '@chakra-ui/react';
+import { RiMenuLine } from 'react-icons/ri';
 import { Logo } from './Logo';
 import { NotificationNav } from './NotificationsNav';
 import { Profile } from './Profile';
 import { SearchBox } from './SearchBox';
 
 export function Header() {
+  const { onOpen } = useSidebarDrawer();
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
@@ -21,6 +25,19 @@ export function Header() {
       px="6"
       align="center"
     >
+      {!isWideVersion && (
+        <IconButton
+          icon={<Icon as={RiMenuLine} />}
+          fontSize="24"
+          variant="unstyled"
+          onClick={onOpen}
+          aria-label="Open navigation"
+          mr="2"
+        >
+
+        </IconButton>
+      )}
+
       <Logo />
 
       {isWideVersion && <SearchBox />}
