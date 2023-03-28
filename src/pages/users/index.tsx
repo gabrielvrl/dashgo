0512bb8835db580ef08673bfd0bb94d68aa8f3b9
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2023-03-28 16:57:53 -0300

    Criando hook de listagem

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -3,33 +3,13 @@ import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead
 import Link from "next/link";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 
-import { useQuery } from '@tanstack/react-query';
-
 import { Header } from '../../components/Header';
 import { Sidebar } from '../../components/Sidebar';
 import { User } from "@/services/mirage";
-import { api } from "@/services/api";
+import { useUsers } from "@/services/hooks/useUsers";
 
 export default function UserList() {
-  const { data, isLoading, isFetching ,error } = useQuery(['users'], async () => {
-    const { data } = await api.get('users');
-
-    const users = data.users.map((user: User) => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-          day: '2-digit',
-          month: 'long',
-          year: 'numeric'
-        })
-      }
-    });
-    return users;
-  }, {
-    staleTime: 1000 * 5, // 5 seconds
-  });
+  const { data, isLoading, isFetching ,error } = useUsers();
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -80,7 +60,7 @@ export default function UserList() {
                   </Thead>
 
                   <Tbody>
-                    {data.map((user: User) => {
+                    {data?.map((user: User) => {
                       return (
                         <Tr key={user.id}>
                           <Td px={["4", "4", "6"]}>
---
+++ src/services/hooks/useUsers.ts
@@ -0,0 +1,29 @@
+import { useQuery } from "@tanstack/react-query";
+import { api } from "../api";
+import { User } from "../mirage";
+
+export async function getUsers():Promise<User[]> {
+    const { data } = await api.get('users');
+  
+    const users = data.users.map((user: User) => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric'
+        })
+      }
+    });
+
+    return users;
+}
+
+export function useUsers() {
+  return useQuery(['users'], getUsers , {
+    staleTime: 1000 * 5, // 5 seconds
+  });
+
+}
